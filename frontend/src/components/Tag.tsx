77fd14d0b467959c8392b9cd0be7c0932eb3454d
knude <105798768+knude@users.noreply.github.com>
2023-09-19 13:16:05 +0300

    remove redundant code

--- frontend/src/components/Tag.tsx
+++ frontend/src/components/Tag.tsx
@@ -1,18 +1,12 @@
-import {Link, useNavigate, useSearchParams} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useQueryClient } from "@tanstack/react-query";
 import { TaggerTag } from "../utils/types";
 
 const Tag = ({ tag }: { tag: TaggerTag }) => {
   const queryClient = useQueryClient();
-  const navigate = useNavigate();
 
   const handleTagClick = async () => {
-    navigate(`/search?q=${tag.name}`);
-
-    await new Promise((resolve) => {
-      setTimeout(resolve, 0);
-    });
-
+    await new Promise((resolve) => setTimeout(resolve, 0));
     await queryClient.invalidateQueries(["files"], { exact: true });
   };
 
